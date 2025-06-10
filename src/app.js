71961c8b6e25b216f24afa16e964464f99325585
sakshimalusare2002 <sakshimalusare10@gmail.com>
2025-06-10 16:37:48 +0530

    new project added
    new project added by sakshi

---
+++ src/app.js
@@ -0,0 +1,17 @@
+let express = require("express");
+let bodyparser = require("body-parser");
+let cors = require("cors");
+require("dotenv").config();
+const db = require("./config/db");
+
+let app = express();
+
+app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// Correct path to your route file
+let userRoutes = require("./routes/user.routes");
+app.use("/", userRoutes);
+
+module.exports = app;
