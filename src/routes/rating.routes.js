c07292898851765cf4b28881c7bf425a62a4f91b
sanika-gavhane <133422020+sanika-gavhane@users.noreply.github.com>
2025-06-20 14:05:03 +0530

    validation

--- src/routes/rating.routes.js
+++ src/routes/rating.routes.js
@@ -1,6 +1,7 @@
 // src/routes/rating.routes.js
 const router = require("express").Router();
 const ratingController = require("../controllers/ratingController");
+router.get('/rate/:movie_id', ratingController.getRatingForm);
 
 router.post("/rate", ratingController.submitRating);
 
